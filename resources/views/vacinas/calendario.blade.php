fbd10589ade4a475c36872f872db8648560fc9eb
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-05-11 15:34:27 -0300

    Ajustar src das imagens na tela de calendarios

--- resources/views/vacinas/calendario.blade.php
+++ resources/views/vacinas/calendario.blade.php
@@ -5,13 +5,13 @@
         <div class="row justify-content-md-center">
             <img class="mt-3 mb-3 img-fluid" src=" {{ asset('images/calendario crianca.png') }}"
                  alt="calendário de vacinação da criança">
-            <img class="mb-3 img-fluid" src="../images/calendario adolescente.png"
+            <img class="mb-3 img-fluid" src="{{ asset('images/calendario adolescente.png')}}"
                  alt="calendário de vacinação do adolescente">
-            <img class="mb-3 img-fluid" src="../images/calendario adulto-idoso.png"
+            <img class="mb-3 img-fluid" src="{{ asset('images/calendario adulto-idoso.png')}}"
                  alt="calendário de vacinação do adulto e idoso">
-            <img class="mb-3 img-fluid" src="../images/calendario gestante.png"
+            <img class="mb-3 img-fluid" src=".{{ asset('images/calendario gestante.png')}}"
                  alt="calendário de vacinação da gestante">
-            <img class="mb-3 img-fluid" src="../images/calendario viajante.png"
+            <img class="mb-3 img-fluid" src="{{ asset('images/calendario viajante.png')}}"
                  alt="calendário de vacinação do viajante">
         </div>
     </div>
